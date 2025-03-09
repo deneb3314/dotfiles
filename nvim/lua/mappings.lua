15ebeb53274863931e76b49e8c7a24c4ed422b81
deneb5678 <ghoskt50@gmail.com>
2025-03-09 22:04:08 +0600

    add nvim confs

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,9 @@
+require "nvchad.mappings"
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({"n","i","v"}, "<C-x>", "<cmd> q <cr>")
+
